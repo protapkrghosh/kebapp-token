ffa0543a6a6bfd76750794c3ef0c89b4db8addcf
Protap Ghosh <protapg518@gmail.com>
2024-03-23 15:05:11 +0600

    initial commit

---
+++ src/Layouts/Main.jsx
@@ -0,0 +1,16 @@
+import Footer from '@/shared/Footer/Footer';
+import NavBar from '@/shared/NavBar/NavBar';
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+const Main = () => {
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+export default Main;
