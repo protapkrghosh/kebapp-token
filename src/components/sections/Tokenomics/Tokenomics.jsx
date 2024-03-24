7f41d1c57b7271bff076124ffb8fe195694b04f7
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-24 11:11:32 +0600

    a

--- src/components/sections/Tokenomics/Tokenomics.jsx
+++ src/components/sections/Tokenomics/Tokenomics.jsx
@@ -8,7 +8,7 @@ const Tokenomics = () => {
   return (
     <div>
       <Container>
-        <div>
+        <div className='mb-[141px]'>
           {/* token title  */}
           <p className='text-[#FFF] text-[58px] font-bold leading-[75.4px] text-center mb-[22px]'>Tokenomics</p>
           <p className='text-[#FFF] text-[18px] leading-[28.4px] text-center mb-20'>Berlin Meme Coin’s tokenomics is a well-balanced structure that ensures sustainability <br /> and growth. Our total token supply is currently divided into various segments</p>
