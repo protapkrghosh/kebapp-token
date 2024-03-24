8e405b9de93b7ae2fb583bc7bb45c3c6422c56a2
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-25 01:17:13 +0600

    howTo done

--- src/components/sections/HowToBuy/HowToBuy.jsx
+++ src/components/sections/HowToBuy/HowToBuy.jsx
@@ -3,7 +3,7 @@ import Container from '../Container/Container';
 import card1logo from '@/assets/howtobuy/Black.png'
 import card2logo from '@/assets/howtobuy/Black (1).png'
 import card3logo from '@/assets/howtobuy/Black (2).png'
-import card4logo from '@/assets/howtobuy/image 198.png'
+import card4logo from '@/assets/howtobuy/icon 41.png'
 
 const HowToBuy = () => {
   return (
@@ -19,45 +19,45 @@ const HowToBuy = () => {
         {/* card contain section  */}
         <div className='flex border-t-[2px] border-[#418077] mb-8'>
           {/* card icon section  */}
-          <div className='xl:w-[50%] flex items-center gap-8'>
+          <div className='lg:w-[50%] flex items-center gap-8'>
             <img src={card1logo} alt="" />
             <p className='text-[28px] font-bold leading-[33.6px]'><span className='bg-clip-text text-transparent bg-gradient-to-l from-[#f2f817] to-[#1bc5df]'>Click on Buy Button</span></p>
           </div>
           {/* card contain section  */}
-          <div className='xl:w-[50%] bg-[#0A1E25] rounded-b-md'>
+          <div className='lg:w-[50%] bg-[#0A1E25] rounded-b-md'>
             <p className='text-[18px] text-[#C7C6C6] xl:max-w-[600px] py-5 pl-[40px]'>Ready to invest in the next big thing? Head to our buy section now and grab your share of the kebapp coin revolution. Don't miss out on the moon ride!</p>
           </div>
         </div>
         <div className='flex border-t-[2px] border-[#418077] mb-8'>
           {/* card icon section  */}
-          <div className='xl:w-[50%] flex items-center gap-8'>
+          <div className='lg:w-[50%] flex items-center gap-8'>
             <img src={card2logo} alt="" />
             <p className='text-[28px] font-bold leading-[33.6px]'><span className='bg-clip-text text-transparent bg-gradient-to-l from-[#f2f817] to-[#1bc5df]'>Connect your Phantom wallet</span></p>
           </div>
           {/* card contain section  */}
-          <div className='xl:w-[50%] bg-[#0A1E25] rounded-b-md'>
+          <div className='lg:w-[50%] bg-[#0A1E25] rounded-b-md'>
             <p className='text-[18px] text-[#C7C6C6] xl:max-w-[600px] py-5 pl-[40px]'>Connect your wallet to join the meme coin revolution! Seamlessly participate in the fun and wild ride of our community-driven token. Don't miss out!</p>
           </div>
         </div>
         <div className='flex border-t-[2px] border-[#418077] mb-8'>
           {/* card icon section  */}
-          <div className='xl:w-[50%] flex items-center gap-8'>
+          <div className='lg:w-[50%] flex items-center gap-8'>
             <img src={card3logo} alt="" />
             <p className='text-[28px] font-bold leading-[33.6px]'><span className='bg-clip-text text-transparent bg-gradient-to-l from-[#f2f817] to-[#1bc5df]'>Choose Amount to Buy</span></p>
           </div>
           {/* card contain section  */}
-          <div className='xl:w-[50%] bg-[#0A1E25] rounded-b-md'>
+          <div className='lg:w-[50%] bg-[#0A1E25] rounded-b-md'>
             <p className='text-[18px] text-[#C7C6C6] xl:max-w-[600px] py-11 pl-[40px]'>Choose the amount to buy and join the meme coin frenzy!</p>
           </div>
         </div>
         <div className='flex border-t-[2px] border-[#418077]'>
           {/* card icon section  */}
-          <div className='xl:w-[50%] flex items-center gap-8'>
+          <div className='lg:w-[50%] flex items-center gap-8'>
             <img src={card4logo} alt="" />
             <p className='text-[28px] font-bold leading-[33.6px]'><span className='bg-clip-text text-transparent bg-gradient-to-l from-[#f2f817] to-[#1bc5df]'>Buy  kebapp token</span></p>
           </div>
           {/* card contain section  */}
-          <div className='xl:w-[50%] bg-[#0A1E25] rounded-b-md'>
+          <div className='lg:w-[50%] bg-[#0A1E25] rounded-b-md'>
             <p className='text-[18px] text-[#C7C6C6] xl:max-w-[600px] py-5 pl-[40px]'>Buy kebapp token and join the meme revolution! Embrace the laughter, fun and potential gains. Secure your spot in the meme coin world. Hurry up!</p>
           </div>
         </div>
