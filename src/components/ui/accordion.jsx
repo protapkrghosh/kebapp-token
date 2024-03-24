9c21144a9c14c3f88f3a15fd9827d2063422bed0
Protap Ghosh <protapg518@gmail.com>
2024-03-24 20:45:49 +0600

    added custom icon

--- src/components/ui/accordion.jsx
+++ src/components/ui/accordion.jsx
@@ -25,11 +25,21 @@ const AccordionTrigger = React.forwardRef(({ className, children, ...props }, re
       {children}
       {/* <ChevronDown className="h-6 w-6 shrink-0 transition-transform duration-200" /> */}
 
+      {/* Minus */}
+      {/* <div className="relative">
+        <div className="w-7 h-7 bg-gradient-to-l from-[#F4F914] to-[#19C5E2] rounded-full flex justify-center items-center">
+          <div className="w-6 h-6 bg-[#0A1E25] rounded-full " />
+        </div>
+        <div className="w-3 h-[3px] bg-gradient-to-l from-[#F4F914] to-[#19C5E2] rounded-md absolute top-3 left-2" />
+      </div> */}
+
+      {/* Plus */}
       <div className="relative">
         <div className="w-7 h-7 bg-gradient-to-l from-[#F4F914] to-[#19C5E2] rounded-full flex justify-center items-center">
           <div className="w-6 h-6 bg-[#0A1E25] rounded-full " />
         </div>
         <div className="w-3 h-[3px] bg-gradient-to-l from-[#F4F914] to-[#19C5E2] rounded-md absolute top-3 left-2" />
+        <div className="w-3 h-[3px] bg-gradient-to-l from-[#19C5E2] to-[#19C5E2] rounded-md absolute top-3 left-2 rotate-90" />
       </div>
 
     </AccordionPrimitive.Trigger>
