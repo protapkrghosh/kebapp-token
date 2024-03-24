2d6418f213e80784d358d301d6541c6aa31580ea
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-24 11:26:28 +0600

    a

--- src/components/sections/Tokenomics/Tokenomics.jsx
+++ src/components/sections/Tokenomics/Tokenomics.jsx
@@ -17,11 +17,11 @@ const Tokenomics = () => {
         {/* tokenomics contain  */}
         <div className='flex'>
           {/* card 1 */}
-          <div className='w-1/2'>
+          <div className='2xl:w-1/2'>
             <div className='mb-[32px] flex justify-between'>
               <div>
                 <p className='text-[24px] font-semibold leading-[33.6px]'><span className='bg-clip-text text-transparent bg-gradient-to-l from-[#f2f817] to-[#1bc5df]'>Seed Round:</span></p>
-                <p className='xl:max-w-[283.206px] text-[#F4F4F4] text-[14px] leading-[22.4px]'>50,000,000 tokens, providing a strong foundation for our project’s early development.</p>
+                <p className='2xl:max-w-[283.206px] text-[#F4F4F4] text-[14px] leading-[22.4px]'>50,000,000 tokens, providing a strong foundation for our project’s early development.</p>
               </div>
 
               <Progress value={33} className="bg-[#F1F715] z-40 xl:max-w-[364.96px]" />
@@ -30,7 +30,7 @@ const Tokenomics = () => {
             <div className='mb-[32px] flex justify-between'>
               <div>
                 <p className='text-[24px] font-semibold leading-[33.6px]'><span className='bg-clip-text text-transparent bg-gradient-to-l from-[#f2f817] to-[#1bc5df]'>Private Round:</span></p>
-                <p className='xl:max-w-[283.206px] text-[#F4F4F4] text-[14px] leading-[22.4px]'>150,000,000 tokens, supporting strategic investors who believe in the long-term potential of Berlin Coin.</p>
+                <p className='2xl:max-w-[283.206px] text-[#F4F4F4] text-[14px] leading-[22.4px]'>150,000,000 tokens, supporting strategic investors who believe in the long-term potential of Berlin Coin.</p>
               </div>
 
               <Progress value={33} className="bg-[#F1F715] z-40 xl:max-w-[364.96px]" />
@@ -39,7 +39,7 @@ const Tokenomics = () => {
             <div className='mb-[32px] flex justify-between'>
               <div>
                 <p className='text-[24px] font-semibold leading-[33.6px]'><span className='bg-clip-text text-transparent bg-gradient-to-l from-[#f2f817] to-[#1bc5df]'>Partners:</span></p>
-                <p className='xl:max-w-[269.235px] text-[#F4F4F4] text-[14px] leading-[22.4px]'>50,000,000 tokens, facilitating partnerships to expand our ecosystem.</p>
+                <p className='2xl:max-w-[269.235px] text-[#F4F4F4] text-[14px] leading-[22.4px]'>50,000,000 tokens, facilitating partnerships to expand our ecosystem.</p>
               </div>
 
               <Progress value={33} className="bg-[#F1F715] z-40 xl:max-w-[364.96px]" />
@@ -48,7 +48,7 @@ const Tokenomics = () => {
             <div className='mb-[32px] flex justify-between'>
               <div>
                 <p className='text-[24px] font-semibold leading-[33.6px]'><span className='bg-clip-text text-transparent bg-gradient-to-l from-[#f2f817] to-[#1bc5df]'>Team & Advisors:</span></p>
-                <p className='xl:max-w-[283.206px] text-[#F4F4F4] text-[14px] leading-[22.4px]'>180,000,000 tokens, incentivizing our talented team and experienced advisors.</p>
+                <p className='2xl:max-w-[283.206px] text-[#F4F4F4] text-[14px] leading-[22.4px]'>180,000,000 tokens, incentivizing our talented team and experienced advisors.</p>
               </div>
 
               <Progress value={33} className="bg-[#F1F715] z-40 xl:max-w-[364.96px]" />
@@ -56,7 +56,7 @@ const Tokenomics = () => {
           </div>
 
           {/* token image */}
-          <div className='xl:w-1/2 relative'>
+          <div className='2xl:w-1/2 relative'>
             <img className='absolute 2xl:bottom-[-70px]' src={tokenomImg} alt="" />
           </div>
         </div>
