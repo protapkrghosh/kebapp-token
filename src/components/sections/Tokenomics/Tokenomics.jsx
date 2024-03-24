bcefc152244cfa2cdea0d4920c5fe82e9498c828
Protap Ghosh <protapg518@gmail.com>
2024-03-25 01:15:13 +0600

    s

--- src/components/sections/Tokenomics/Tokenomics.jsx
+++ src/components/sections/Tokenomics/Tokenomics.jsx
@@ -6,7 +6,7 @@ import tokenomImg from '@/assets/tokenomics/Group 1000003525.png'
 
 const Tokenomics = () => {
   return (
-    <div>
+    <div className='pb-20'>
       <Container>
         <div className='mb-[141px]'>
           {/* token title  */}
