081035e6757835b2c74d9af79126d5ff74a2c39a
Protap Ghosh <protapg518@gmail.com>
2024-03-24 14:56:07 +0600

    added roadmap section

--- src/components/sections/Roadmap/Roadmap.jsx
+++ src/components/sections/Roadmap/Roadmap.jsx
@@ -5,12 +5,13 @@ import { FaCheck } from "react-icons/fa6";
 
 const Roadmap = () => {
   return (
-    <div>
+    <div className="pt-32 pb-52 bg-[#040D1B]">
       <Container>
         <div>
           <h1 className="text-white text-[62px] text-center font-normal">Roadmap</h1>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mb-9">
+          {/* Phase */}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-16 mb-9">
             <p className='text-[24px] bg-clip-text text-transparent bg-gradient-to-l from-[#F4F914] to-[#19C5E2] font-bold'>Phase - 1</p>
 
             <p className='text-[24px] bg-clip-text text-transparent bg-gradient-to-l from-[#F4F914] to-[#19C5E2] font-bold'>Phase - 2</p>
@@ -20,34 +21,39 @@ const Roadmap = () => {
 
           {/* Custom border */}
           <div className="relative">
-            <img src={borderImg} alt="Image" className="w-full" />
-            <div className="absolute top-0">
-              <img src={lineBorderImg} alt="Image" className="" />
-              <div className=" absolute top-[-15px] left-[-12px]">
-                <div className="w-7 h-7 rounded-full bg-gradient-to-l from-[#F4F914] to-[#19C5E2]" />
-                <FaCheck className="text-black absolute top-1.5 left-1.5"/>
-              </div>
-            </div>
+            <div className="absolute w-full">
+              <div>
+                <img src={borderImg} alt="Image" className="w-full" />
+                <div className="absolute top-0">
+                  <img src={lineBorderImg} alt="Image" className="" />
+                  <div className=" absolute top-[-15px] left-[-12px]">
+                    <div className="w-7 h-7 rounded-full bg-gradient-to-l from-[#F4F914] to-[#19C5E2]" />
+                    <FaCheck className="text-black absolute top-1.5 left-1.5" />
+                  </div>
+                </div>
 
-            <div className="absolute top-0 left-96 2xl:left-[475px]">
-              <img src={lineBorderImg} alt="Image" className="" />
-              <div className=" absolute top-[-15px] left-[-12px]">
-                <div className="w-7 h-7 rounded-full bg-gradient-to-l from-[#F4F914] to-[#19C5E2]" />
-                <FaCheck className="text-black absolute top-1.5 left-1.5"/>
-              </div>
-            </div>
+                <div className="absolute top-0 left-96 2xl:left-[475px]">
+                  <img src={lineBorderImg} alt="Image" className="" />
+                  <div className=" absolute top-[-15px] left-[-12px]">
+                    <div className="w-7 h-7 rounded-full bg-gradient-to-l from-[#F4F914] to-[#19C5E2]" />
+                    <FaCheck className="text-black absolute top-1.5 left-1.5" />
+                  </div>
+                </div>
 
-            <div className="absolute top-0 left-[770px] 2xl:left-[955px]">
-              <img src={lineBorderImg} alt="Image" className="" />
-              <div className=" absolute top-[-15px] left-[-12px]">
-                <div className="w-7 h-7 rounded-full bg-gradient-to-l from-[#F4F914] to-[#19C5E2]" />
-                <FaCheck className="text-black absolute top-1.5 left-1.5"/>
+                <div className="absolute top-0 left-[770px] 2xl:left-[955px]">
+                  <img src={lineBorderImg} alt="Image" className="" />
+                  <div className=" absolute top-[-15px] left-[-12px]">
+                    <div className="w-7 h-7 rounded-full bg-gradient-to-l from-[#F4F914] to-[#19C5E2]" />
+                    <FaCheck className="text-black absolute top-1.5 left-1.5" />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
 
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-            <div className="space-y-4 pl-5">
+            {/* Phase One */}
+            <div className="space-y-4 ml-9 mt-9">
               <div className="flex items-center">
                 <div className="w-3 h-3 bg-gradient-to-l from-[#F4F914] to-[#19C5E2] rounded-full mr-3" />
                 <p className="text-white text-[20px] font-normal">Launch on Solana</p>
@@ -66,6 +72,52 @@ const Roadmap = () => {
               <div className="flex items-center">
                 <div className="w-3 h-3 bg-gradient-to-l from-[#F4F914] to-[#19C5E2] rounded-full mr-3" />
                 <p className="text-white text-[20px] font-normal">2500+ Holders</p>
+              </div>
+            </div>
+
+            {/* Phase Two */}
+            <div className="space-y-4 ml-9 mt-9">
+              <div className="flex items-center">
+                <div className="w-3 h-3 bg-gradient-to-l from-[#F4F914] to-[#19C5E2] rounded-full mr-3" />
+                <p className="text-white text-[20px] font-normal">Public Presale</p>
+              </div>
+
+              <div className="flex items-center">
+                <div className="w-3 h-3 bg-gradient-to-l from-[#F4F914] to-[#19C5E2] rounded-full mr-3" />
+                <p className="text-white text-[20px] font-normal">Dexscreener and Birdseye Update</p>
+              </div>
+
+              <div className="flex items-center">
+                <div className="w-3 h-3 bg-gradient-to-l from-[#F4F914] to-[#19C5E2] rounded-full mr-3" />
+                <p className="text-white text-[20px] font-normal">$20M+ Daily Volume</p>
+              </div>
+
+              <div className="flex items-center">
+                <div className="w-3 h-3 bg-gradient-to-l from-[#F4F914] to-[#19C5E2] rounded-full mr-3" />
+                <p className="text-white text-[20px] font-normal">10000+ Holders</p>
+              </div>
+            </div>
+
+            {/* Phase Three */}
+            <div className="space-y-4 ml-9 mt-9">
+              <div className="flex items-center">
+                <div className="w-3 h-3 bg-gradient-to-l from-[#F4F914] to-[#19C5E2] rounded-full mr-3" />
+                <p className="text-white text-[20px] font-normal">Trending on X</p>
+              </div>
+
+              <div className="flex items-center">
+                <div className="w-3 h-3 bg-gradient-to-l from-[#F4F914] to-[#19C5E2] rounded-full mr-3" />
+                <p className="text-white text-[20px] font-normal">CG CMC Listing</p>
+              </div>
+
+              <div className="flex items-center">
+                <div className="w-3 h-3 bg-gradient-to-l from-[#F4F914] to-[#19C5E2] rounded-full mr-3" />
+                <p className="text-white text-[20px] font-normal">NFT Airdrop</p>
+              </div>
+
+              <div className="flex items-center">
+                <div className="w-3 h-3 bg-gradient-to-l from-[#F4F914] to-[#19C5E2] rounded-full mr-3" />
+                <p className="text-white text-[20px] font-normal">100000+ Holders</p>
               </div>
             </div>
           </div>
