f3b831cd08c9beae3d3aa48da649eb66141c42cd
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-25 11:32:48 +0600

    aa

--- src/components/sections/Tokenomics/Tokenomics.jsx
+++ src/components/sections/Tokenomics/Tokenomics.jsx
@@ -17,7 +17,8 @@ const Tokenomics = () => {
         {/* tokenomics contain  */}
         <div className='flex'>
           {/* card 1 */}
-          <div className='xl:w-1/2'>
+          <div className='xl:w-[60%]'>
+
             <div className='mb-[32px] flex justify-between space-x-14'>
               <div>
                 <p className='text-[24px] font-semibold leading-[33.6px]'><span className='bg-clip-text text-transparent bg-gradient-to-l from-[#f2f817] to-[#1bc5df]'>Seed Round:</span></p>
@@ -25,10 +26,35 @@ const Tokenomics = () => {
               </div>
               <Progress value={60} className="z-40" clsName="bg-[#F1F715]" />
             </div>
+
+            <div className='mb-[32px] flex justify-between space-x-14'>
+              <div>
+                <p className='text-[24px] font-semibold leading-[33.6px]'><span className='bg-clip-text text-transparent bg-gradient-to-l from-[#f2f817] to-[#1bc5df]'>Private Round:</span></p>
+                <p className='text-[#F4F4F4] text-[14px] leading-[22.4px]'>150,000,000 tokens, supporting strategic investors who believe in the long-term potential of Berlin Coin.</p>
+              </div>
+              <Progress value={30} className="z-40" clsName="bg-[#21C6D9]" />
+            </div>
+
+            <div className='mb-[32px] flex justify-between space-x-14'>
+              <div>
+                <p className='text-[24px] font-semibold leading-[33.6px]'><span className='bg-clip-text text-transparent bg-gradient-to-l from-[#f2f817] to-[#1bc5df]'>Partners:</span></p>
+                <p className='text-[#F4F4F4] text-[14px] leading-[22.4px]'>50,000,000 tokens, facilitating partnerships to expand our ecosystem.</p>
+              </div>
+              <Progress value={30} className="z-40" clsName="bg-[#C060A1]" />
+            </div>
+
+            <div className='mb-[32px] flex justify-between space-x-14'>
+              <div>
+                <p className='text-[24px] font-semibold leading-[33.6px]'><span className='bg-clip-text text-transparent bg-gradient-to-l from-[#f2f817] to-[#1bc5df]'>Team & Advisors:</span></p>
+                <p className='text-[#F4F4F4] text-[14px] leading-[22.4px]'>180,000,000 tokens, incentivizing our talented team and experienced advisors.</p>
+              </div>
+              <Progress value={50} className="z-40" clsName="bg-[#6D67E4]" />
+            </div>
+
           </div>
 
           {/* token image */}
-          <div className='xl:w-1/2'>
+          <div className='xl:w-[40%]'>
             <img className=' 2xl:bottom-[-90px] xl:right-[-90px]' src={tokenomImg} alt="" />
           </div>
         </div>
