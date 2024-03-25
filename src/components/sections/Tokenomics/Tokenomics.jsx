801eca325c8c1ab037cad3334e2b2266e4b9e0bb
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-25 11:12:42 +0600

    aa

--- src/components/sections/Tokenomics/Tokenomics.jsx
+++ src/components/sections/Tokenomics/Tokenomics.jsx
@@ -17,19 +17,19 @@ const Tokenomics = () => {
         {/* tokenomics contain  */}
         <div className='flex'>
           {/* card 1 */}
-          <div className='2xl:w-1/2'>
-            <div className='mb-[32px] flex justify-between'>
+          <div className='xl:w-1/2'>
+            <div className='mb-[32px] flex justify-between space-x-14'>
               <div>
                 <p className='text-[24px] font-semibold leading-[33.6px]'><span className='bg-clip-text text-transparent bg-gradient-to-l from-[#f2f817] to-[#1bc5df]'>Seed Round:</span></p>
-                <p className='2xl:max-w-[283.206px] text-[#F4F4F4] text-[14px] leading-[22.4px]'>50,000,000 tokens, providing a strong foundation for our project’s early development.</p>
+                <p className='text-[#F4F4F4] text-[14px] leading-[22.4px]'>50,000,000 tokens, providing a strong foundation for our project’s early development.</p>
               </div>
 
-              <Progress value={75} className="bg-[#F1F715] z-40 xl:max-w-[364.96px]" />
+              <Progress value={75} className="bg-[#F1F715] z-40" />
             </div>
           </div>
 
           {/* token image */}
-          <div className='2xl:w-1/2 '>
+          <div className='xl:w-1/2 '>
             <img className=' 2xl:bottom-[-90px] 2xl:right-[-90px]' src={tokenomImg} alt="" />
           </div>
         </div>
