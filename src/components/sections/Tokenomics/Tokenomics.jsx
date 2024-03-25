18cb16b311d0162155339020954a577c1577dcd3
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-25 12:33:36 +0600

    aa

--- src/components/sections/Tokenomics/Tokenomics.jsx
+++ src/components/sections/Tokenomics/Tokenomics.jsx
@@ -19,27 +19,30 @@ const Tokenomics = () => {
           {/* card 1 */}
           <div className='xl:w-[60%]'>
 
-            <div className='mb-[32px] flex justify-between'>
+            <div className='mb-[32px] flex justify-between relative'>
               <div className='2xl:w-[30%]'>
                 <p className='text-[24px] font-semibold leading-[33.6px]'><span className='bg-clip-text text-transparent bg-gradient-to-l from-[#f2f817] to-[#1bc5df]'>Seed Round:</span></p>
                 <p className='text-[#F4F4F4] text-[14px] leading-[22.4px]'>50,000,000 tokens, providing a strong foundation for our project’s early development.</p>
               </div>
+              <p className='text-[#FFF] text-[14.833px] font-semibold leading-[17.8px] absolute 2xl:bottom-[45px] 2xl:left-[380px] z-50'>50%</p>
               <Progress value={60} className="z-40 2xl:w-[60%]" clsName="bg-[#F1F715]" />
             </div>
 
-            <div className='mb-[32px] flex justify-between'>
+            <div className='mb-[32px] flex justify-between relative'>
               <div className='2xl:w-[34%]'>
                 <p className='text-[24px] font-semibold leading-[33.6px]'><span className='bg-clip-text text-transparent bg-gradient-to-l from-[#f2f817] to-[#1bc5df]'>Private Round:</span></p>
                 <p className='text-[#F4F4F4] text-[14px] leading-[22.4px]'>150,000,000 tokens, supporting strategic investors who believe in the long-term potential of Berlin Coin.</p>
               </div>
+              <p className='text-[#FFF] text-[14.833px] font-semibold leading-[17.8px] absolute 2xl:bottom-[45px] 2xl:left-[380px] z-50'>50%</p>
               <Progress value={30} className="z-40 2xl:w-[60%]" clsName="bg-[#21C6D9]" />
             </div>
 
-            <div className='mb-[32px] flex justify-between 2xl:gap-20'>
+            <div className='mb-[32px] flex justify-between 2xl:gap-20 relative'>
               <div className='2xl:max-w-[269.235px]'>
                 <p className='text-[24px] font-semibold leading-[33.6px]'><span className='bg-clip-text text-transparent bg-gradient-to-l from-[#f2f817] to-[#1bc5df]'>Partners:</span></p>
                 <p className='text-[#F4F4F4] text-[14px] leading-[22.4px]'>50,000,000 tokens, facilitating partnerships to expand our ecosystem.</p>
               </div>
+              <p className='text-[#FFF] text-[14.833px] font-semibold leading-[17.8px] absolute 2xl:bottom-[45px] 2xl:left-[380px] z-50'>50%</p>
               <Progress value={30} className="z-40 2xl:max-w-[364.96px]'>" clsName="bg-[#C060A1]" />
             </div>
 
