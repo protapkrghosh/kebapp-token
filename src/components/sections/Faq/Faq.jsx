5541a45d317fc4547229f1a9d6d79ffc4fc73f96
Protap Ghosh <protapg518@gmail.com>
2024-03-25 16:27:23 +0600

    update p color

--- src/components/sections/Faq/Faq.jsx
+++ src/components/sections/Faq/Faq.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
 import Container from '../Container/Container';
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 
 const Faq = () => {
+
   return (
     <div className='mt-16 mb-32'>
       <Container>
@@ -12,35 +12,35 @@ const Faq = () => {
           <Accordion type="single" defaultValue="item-1" collapsible className="w-full">
             <AccordionItem value="item-1">
               <AccordionTrigger>What is KEBAPP Heaven?</AccordionTrigger>
-              <AccordionContent className="xl:w-[76%]">
+              <AccordionContent className="xl:w-[76%] text-[#b4b8ba]">
                 KEBAPP Heaven is a decentralized finance (DeFi) platform built on the Binance Smart Chain (BSC). It offers a variety of DeFi services, including yield farming, liquidity provision, and token swaps, all centered around our native token, KEBAPP.
               </AccordionContent>
             </AccordionItem>
 
             <AccordionItem value="item-2">
               <AccordionTrigger>How do I participate in KEBAPP Heaven?</AccordionTrigger>
-              <AccordionContent className="xl:w-[72%]">
+              <AccordionContent className="xl:w-[72%] text-[#b4b8ba]">
                 To participate in KEBAPP Heaven, you'll need to connect your BSC-compatible wallet, such as MetaMask or Trust Wallet, to our platform. From there, you can engage in various DeFi activities like farming, staking, and swapping tokens.
               </AccordionContent>
             </AccordionItem>
 
             <AccordionItem value="item-3">
               <AccordionTrigger>What is KEBAPP Token?</AccordionTrigger>
-              <AccordionContent className="xl:w-[60%]">
+              <AccordionContent className="xl:w-[60%] text-[#b4b8ba]">
                 KEBAPP Token is our platform's native cryptocurrency. It serves multiple purposes within the KEBAPP Heaven ecosystem, including governance, yield farming rewards, and liquidity incentives.
               </AccordionContent>
             </AccordionItem>
 
             <AccordionItem value="item-4">
               <AccordionTrigger>How can I acquire KEBAPP Tokens?</AccordionTrigger>
-              <AccordionContent className="xl:w-[69%]">
+              <AccordionContent className="xl:w-[69%] text-[#b4b8ba]">
                 You can acquire KEBAPP Tokens by swapping other cryptocurrencies for KEBAPP on decentralized exchanges (DEXs) like PancakeSwap. Make sure to have Binance Coin (BNB) or another supported token to trade for KEBAPP.
               </AccordionContent>
             </AccordionItem>
 
             <AccordionItem value="item-5">
               <AccordionTrigger>What are the benefits of holding KEBAPP Tokens?</AccordionTrigger>
-              <AccordionContent className="xl:w-[64%]">
+              <AccordionContent className="xl:w-[64%] text-[#b4b8ba]">
                 Holding KEBAPP Tokens provides several benefits, including voting rights in governance decisions, earning rewards through yield farming, and participating in community events and initiatives.
               </AccordionContent>
             </AccordionItem>
--- src/components/sections/Roadmap/Roadmap.jsx
+++ src/components/sections/Roadmap/Roadmap.jsx
@@ -59,22 +59,22 @@ const Roadmap = () => {
               <div className="space-y-2 xl:space-y-4 ml-6 xl:ml-9 mt-7 xl:mt-9">
                 <div className="flex items-center">
                   <div className="w-3 h-3 bg-gradient-to-l from-[#F4F914] to-[#19C5E2] rounded-full mr-3" />
-                  <p className="text-white xl:text-[20px] font-normal">Launch on Solana</p>
+                  <p className="text-[#ced1d2] xl:text-[20px] font-normal">Launch on Solana</p>
                 </div>
 
                 <div className="flex items-center">
                   <div className="w-3 h-3 bg-gradient-to-l from-[#F4F914] to-[#19C5E2] rounded-full mr-3" />
-                  <p className="text-white xl:text-[20px] font-normal">Website Landing Page</p>
+                  <p className="text-[#ced1d2] xl:text-[20px] font-normal">Website Landing Page</p>
                 </div>
 
                 <div className="flex items-center">
                   <div className="w-3 h-3 bg-gradient-to-l from-[#F4F914] to-[#19C5E2] rounded-full mr-3" />
-                  <p className="text-white xl:text-[20px] font-normal">Presale</p>
+                  <p className="text-[#ced1d2] xl:text-[20px] font-normal">Presale</p>
                 </div>
 
                 <div className="flex items-center">
                   <div className="w-3 h-3 bg-gradient-to-l from-[#F4F914] to-[#19C5E2] rounded-full mr-3" />
-                  <p className="text-white xl:text-[20px] font-normal">2500+ Holders</p>
+                  <p className="text-[#ced1d2] xl:text-[20px] font-normal">2500+ Holders</p>
                 </div>
               </div>
 
@@ -85,22 +85,22 @@ const Roadmap = () => {
                 <div className="space-y-2 xl:space-y-4 ml-6 xl:ml-9 mt-[68px] lg:mt-7 xl:mt-9">
                   <div className="flex items-center">
                     <div className="w-3 h-3 bg-gradient-to-l from-[#F4F914] to-[#19C5E2] rounded-full mr-3" />
-                    <p className="text-white xl:text-[20px] font-normal">Public Presale</p>
+                    <p className="text-[#ced1d2] xl:text-[20px] font-normal">Public Presale</p>
                   </div>
 
                   <div className="flex items-center">
                     <div className="w-3 h-3 bg-gradient-to-l from-[#F4F914] to-[#19C5E2] rounded-full mr-3" />
-                    <p className="text-white xl:text-[20px] font-normal">Dexscreener and Birdseye Update</p>
+                    <p className="text-[#ced1d2] xl:text-[20px] font-normal">Dexscreener and Birdseye Update</p>
                   </div>
 
                   <div className="flex items-center">
                     <div className="w-3 h-3 bg-gradient-to-l from-[#F4F914] to-[#19C5E2] rounded-full mr-3" />
-                    <p className="text-white xl:text-[20px] font-normal">$20M+ Daily Volume</p>
+                    <p className="text-[#ced1d2] xl:text-[20px] font-normal">$20M+ Daily Volume</p>
                   </div>
 
                   <div className="flex items-center">
                     <div className="w-3 h-3 bg-gradient-to-l from-[#F4F914] to-[#19C5E2] rounded-full mr-3" />
-                    <p className="text-white xl:text-[20px] font-normal">10000+ Holders</p>
+                    <p className="text-[#ced1d2] xl:text-[20px] font-normal">10000+ Holders</p>
                   </div>
                 </div>
               </div>
@@ -112,22 +112,22 @@ const Roadmap = () => {
                 <div className="space-y-2 xl:space-y-4 ml-6 xl:ml-9 mt-[68px] lg:mt-7 xl:mt-9">
                   <div className="flex items-center">
                     <div className="w-3 h-3 bg-gradient-to-l from-[#F4F914] to-[#19C5E2] rounded-full mr-3" />
-                    <p className="text-white xl:text-[20px] font-normal">Trending on X</p>
+                    <p className="text-[#ced1d2] xl:text-[20px] font-normal">Trending on X</p>
                   </div>
 
                   <div className="flex items-center">
                     <div className="w-3 h-3 bg-gradient-to-l from-[#F4F914] to-[#19C5E2] rounded-full mr-3" />
-                    <p className="text-white xl:text-[20px] font-normal">CG CMC Listing</p>
+                    <p className="text-[#ced1d2] xl:text-[20px] font-normal">CG CMC Listing</p>
                   </div>
 
                   <div className="flex items-center">
                     <div className="w-3 h-3 bg-gradient-to-l from-[#F4F914] to-[#19C5E2] rounded-full mr-3" />
-                    <p className="text-white xl:text-[20px] font-normal">NFT Airdrop</p>
+                    <p className="text-[#ced1d2] xl:text-[20px] font-normal">NFT Airdrop</p>
                   </div>
 
                   <div className="flex items-center">
                     <div className="w-3 h-3 bg-gradient-to-l from-[#F4F914] to-[#19C5E2] rounded-full mr-3" />
-                    <p className="text-white xl:text-[20px] font-normal">100000+ Holders</p>
+                    <p className="text-[#ced1d2] xl:text-[20px] font-normal">100000+ Holders</p>
                   </div>
                 </div>
               </div>
