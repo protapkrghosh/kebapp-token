83f1e10d9f87e82d9fa1c5e94eb8759ff314000d
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-25 13:26:40 +0600

    aa

--- src/components/sections/Tokenomics/Tokenomics.jsx
+++ src/components/sections/Tokenomics/Tokenomics.jsx
@@ -37,13 +37,13 @@ const Tokenomics = () => {
               <Progress value={30} className="z-40 2xl:w-[60%] xl:w-[55%]" clsName="bg-[#21C6D9]" />
             </div>
 
-            <div className='mb-[32px] flex justify-between 2xl:gap-[80px] relative'>
-              <div className='2xl:max-w-[269.235px]'>
+            <div className='mb-[32px] flex justify-between 2xl:gap-[80px] xl:gap-[67px] relative'>
+              <div className='2xl:max-w-[269.235px] xl:max-w-[269.235px]'>
                 <p className='text-[24px] font-semibold leading-[33.6px]'><span className='bg-clip-text text-transparent bg-gradient-to-l from-[#f2f817] to-[#1bc5df]'>Partners:</span></p>
                 <p className='text-[#F4F4F4] text-[14px] leading-[22.4px]'>50,000,000 tokens, facilitating partnerships to expand our ecosystem.</p>
               </div>
               <p className='text-[#FFF] text-[14.833px] font-semibold leading-[17.8px] absolute 2xl:bottom-[45px] 2xl:left-[380px] z-50'>50%</p>
-              <Progress value={30} className="z-40 2xl:max-w-[364.96px]'>" clsName="bg-[#C060A1]" />
+              <Progress value={30} className="z-40 2xl:max-w-[364.96px] xl:max-w-[364.96px]'>" clsName="bg-[#C060A1]" />
             </div>
 
             <div className='mb-[32px] flex justify-between relative'>
